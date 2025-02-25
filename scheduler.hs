635cfe6b5f57c1603914be2153e43cdfa3ab053e
wowitsalison <aliatmra@gmail.com>
2025-02-24 21:09:53 -0600

    Restructure program to better utilize functional programming

--- scheduler.hs
+++ scheduler.hs
@@ -1,87 +1,76 @@
 import System.IO (readFile)
-import Data.List (words)
+import Data.List (words, minimumBy)
+import Data.Ord (comparing)
 import Data.Char (chr)
 import Control.Monad (forM_, when)
 import Control.Concurrent (threadDelay)
-import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime)
 
--- Record to hold each input arrival time, priority, and cpu time
+-- Record to hold each process input
 data Input = Input
-    { id       :: Char,
+    { charId   :: Char,
       arrival  :: Int,
       priority :: Int,
-      cpu_time :: Int
+      cpuTime  :: Int
     } deriving (Show)
 
-readyQueue :: [Int] 
-readyQueue = []
-
 -- Puts each string of arrival, priority, and cpu time into record
-parseInput :: String -> Input
-parseInput line = 
+parseInput :: Int -> String -> Input
+parseInput index line = 
     let [a, p, c] = map read (words line)
-        id = chr (index + 65)
-    in Input id a p c
-
--- Remove head from queue
-removeFirstInQueue :: [a] -> [a]
-removeFirstInQueue [] = []
-removeFirstInQueue (x:xs) = xs
-
--- Remove head from queue then replace it in the back
-replaceInQueue :: [a] -> [a]
-replaceInQueue [] = []
-replaceInQueue (x:xs) = xs ++ [x]
+    in Input (chr (index + 65)) a p c
 
 -- Find the input with the lowest priority in the ready queue
-findLowestPriority :: [Input] -> Input
-findLowestPriority queue = minimumBy (comparing priority) queue
+findLowestPriority :: [Input] -> Maybe Input
+findLowestPriority [] = Nothing
+findLowestPriority queue = Just (minimumBy (comparing priority) queue)
 
--- Check to see if the timer matches a value (arrival time)
-checkTimer :: Int -> Int -> Bool
-checkTimer x timer = x == timer
-
-startTimer :: IO ()
-startTimer = do
-    startTime <- getCurrentTime
-    putStrLn "START"
-
-scheduler :: [Input] -> [Input] -> Int -> IO ()
+-- Run one second of the scheduler
+scheduler :: UTCTime -> [Input] -> [Input] -> IO ()
 scheduler startTime inputs readyQueue = do
-    if null readyQueue && null inputs then
-        putStrLn "END"
-    else do
-        currentTime <- getCurrentTime
-        let seconds = floor (diffUTCTime currentTime startTime) :: Int
-        let (x:xs) = inputs
+    currentTime <- getCurrentTime
+    let seconds = floor (diffUTCTime currentTime startTime) :: Int
+    let (x:xs) = inputs
+    
+    -- Add to ready queue
+    let (newArrivals, remainingInputs) = span (\p -> arrival p == seconds) inputs
+    let updatedQueue = readyQueue ++ newArrivals
         
-        -- Check if the arrival time matches the timer's time
-        if arrival x == seconds then do
-            -- Add to ready queue and remove from 
-            let updatedReadyQueue = readyQueue ++ [x]
-            let updatedInputs = xs
-            
-            -- Check priority in the ready queue
-            let lowestPriority = findLowestPriority readyQueue
-            if cpu_time > 0 then
-                print seconds
-                print id lowestPriority
-                cpu_time lowestPriority = cpu_time lowestPriority - 1
+    case findLowestPriority updatedQueue of
+        Just process ->
+            if cpuTime process > 0 then do
+                putStrLn $ show seconds ++ [charId process]
+                let updatedQueue = updateQueue process updatedQueue
+                threadDelay 1000000 -- Delay one second
+                scheduler startTime remainingInputs updatedQueue
+            else
+                scheduler startTime remainingInputs (removeFromQueue process updatedQueue)
+        Nothing ->
+            if null remainingInputs then putStrLn "END"
             else do
-                -- Remove lowestPriority from readyQueue
+                print seconds
+                threadDelay 1000000 -- Delay one second
+                scheduler startTime remainingInputs updatedQueue
 
-            -- Round robin if equal priority
+-- 
+updateQueue :: Input -> [Input] -> [Input]
+updateQueue process queue =
+    let updatedProcess = process { cpuTime = cpuTime process - 1}
+    in replaceInQueue updatedProcess queue
 
-            print updatedReadyQueue
-            scheduler startTime updatedInputs updatedReadyQueue
-        else 
-            print seconds
-            threadDelay 1000000  -- 1 second delay
-            scheduler startTime inputs readyQueue
+-- 
+removeFromQueue :: Input -> [Input] -> [Input]
+removeFromQueue process = filter (\p -> charId p /= charId process)
+
+replaceInQueue :: Input -> [Input] -> [Input]
+replaceInQueue process queue =
+    let withoutProcess = removeFromQueue process queue
+    in withoutProcess ++ [process]
             
 main :: IO ()
 main = do
     contents <- readFile "input.txt" -- placeholder filename
     let inputs = zipWith parseInput [0..] (lines contents)
-    startTime = startTimer
-    scheduler startTime inputs readyQueue
+    startTime <- getCurrentTime
+    putStrLn "START"
+    scheduler startTime inputs []
